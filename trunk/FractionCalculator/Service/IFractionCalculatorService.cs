0c43e0359bea2ce29a9ea5531d49534a491e838e
eric.eijkelenboom@gmail.com <eric.eijkelenboom@gmail.com@5ea50fbe-93e7-1351-8041-2e62c6d872af>
2011-09-26 07:53:32 +0000

    Added comments to the service interface

--- trunk/FractionCalculator/Service/IFractionCalculatorService.cs
+++ trunk/FractionCalculator/Service/IFractionCalculatorService.cs
@@ -4,9 +4,36 @@ namespace FractionCalculator.Service
 {
     public interface IFractionCalculatorService
     {
+        /// <summary>
+        /// Add to fractions
+        /// </summary>
+        /// <param name="fraction1">The first fraction</param>
+        /// <param name="fraction2">The second fraction</param>
+        /// <returns>A fraction containing the result of the addition</returns>
         Fraction Add(Fraction fraction1, Fraction fraction2);
+
+        /// <summary>
+        /// Subtract two fractions
+        /// </summary>
+        /// <param name="fraction1">The first fraction</param>
+        /// <param name="fraction2">The second fraction</param>
+        /// <returns><returns>A fraction containing the result of the subtraction</returns></returns>
         Fraction Subtract(Fraction fraction1, Fraction fraction2);
+
+        /// <summary>
+        /// Divide two fractions
+        /// </summary>
+        /// <param name="fraction1">The first fraction</param>
+        /// <param name="fraction2">The second fraction</param>
+        /// <returns><returns>A fraction containing the result of the division</returns></returns>
         Fraction Divide(Fraction fraction1, Fraction fraction2);
+
+        /// <summary>
+        /// Multiply two fractions
+        /// </summary>
+        /// <param name="fraction1">The first fraction</param>
+        /// <param name="fraction2">The second fraction</param>
+        /// <returns><returns>A fraction containing the result of the multiplication</returns></returns>
         Fraction Multiply(Fraction fraction1, Fraction fraction2);
     }
 }
